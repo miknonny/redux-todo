4f1cfa03d03094bea2974ffe4ebd0ca1c1e4671e
nonnycode <miknonny@gmail.com>
2016-01-17 19:03:36 +0100

    added redux composition

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -19,18 +19,20 @@ export const VisibilityFilters = {
 /*
  * Action creators
  */
+let nextTodoId = 0;
 
 export const addTodo = (text) => {
   return {
     type: ADD_TODO,
+    id: nextTodoId++,
     text
   }
 }
 
-export const completeTodo = (index) => {
+export const completeTodo = (id) => {
   return {
     type: COMPLETE_TODO,
-    index
+    id
   }
 }
 
